809e17a946144d26c342502e84ee650754cf0e63
Satendra <thewarriormonkk@users.noreply.github.com>
2026-01-20 21:38:37 +0530

    feat(ui): highlight invalid fields from backend backend validation

--- client/src/components/AddProduct.jsx
+++ client/src/components/AddProduct.jsx
@@ -8,6 +8,8 @@ const AddProduct = () => {
     const [color, setColor] = useState('');
     const [dimension, setDimension] = useState('');
     const [countryOfOrigin, setCountryOfOrigin] = useState('');
+    const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const { dispatch } = useProductsContext();
 
@@ -28,7 +30,14 @@ const AddProduct = () => {
         });
 
         const data = await response.json();
+
+        if (!response.ok) {
+            setError(data.message);
+            setEmptyFields(data.emptyFields);
+        }
+
         if (response.ok) {
+            setEmptyFields([]);
             setProductName('');
             setPrice('');
             setColor('');
@@ -37,7 +46,6 @@ const AddProduct = () => {
             
             dispatch({ type: 'CREATE_PRODUCT', payload: data })
             navigate('/catalog');
-
         }
     }
 
@@ -50,18 +58,21 @@ const AddProduct = () => {
                 type="text"
                 onChange={(e) => setProductName(e.target.value)}
                 value={productName}
+                className={emptyFields.includes('productName') ? 'error' : '' }
             />
             <label>Price: </label>
             <input 
                 type="text"
                 onChange={(e) => setPrice(e.target.value)}
                 value={price}
+                className={emptyFields.includes('price') ? 'error' : '' }
             />
             <label>Dimension: </label>
             <input 
                 type="text"
                 onChange={(e) => setDimension(e.target.value)}
                 value={dimension}
+                className={emptyFields.includes('dimension') ? 'error' : '' }
             />
 
             <label>Color: </label>
@@ -69,6 +80,7 @@ const AddProduct = () => {
                 type="text"
                 onChange={(e) => setColor(e.target.value)}
                 value={color}
+                className={emptyFields.includes('color') ? 'error' : '' }
             />
 
             <label>Country: </label>
@@ -76,8 +88,10 @@ const AddProduct = () => {
                 type="text"
                 onChange={(e) => setCountryOfOrigin(e.target.value)}
                 value={countryOfOrigin}
+                className={emptyFields.includes('countryOfOrigin') ? 'error' : '' }
             />
             <button>Add Product</button>
+            {error && <div className='error'>{ error }</div>}
         </form>
     );
 }
